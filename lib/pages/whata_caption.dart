2aeb6a1eaad7260859e725e670b0fba8d97c220b
aswatson <aswatson95@gmail.com>
2023-11-08 21:52:06 -0700

    fixed issues in branch

--- lib/pages/whata_caption.dart
+++ lib/pages/whata_caption.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class WhataCaptionPage extends StatefulWidget{
   const WhataCaptionPage({super.key});
@@ -11,6 +13,24 @@ class WhataCaptionPage extends StatefulWidget{
 }
 
 class _WhataCaptionPageState extends State<WhataCaptionPage>{
+  final TextEditingController _textEditingController = TextEditingController();
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  int _votes = 0;
+  static File? _imageFile;
+
+  Future<void> _findImageFile() async {
+    ImagePicker picker = ImagePicker();
+    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      _imageFile = File(image!.path);
+    });
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context){
@@ -19,14 +39,18 @@ class _WhataCaptionPageState extends State<WhataCaptionPage>{
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Join Game"),
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Row(
-              children: <Widget>[
-                Text('Score: '),
-              ],
+            Text('Score: $_votes', style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+            const SizedBox(height: 10),
+            ElevatedButton.icon(
+              onPressed: () {
+                _findImageFile().then((_) => null);
+              },
+              icon: const Icon(Icons.cloud_upload),
+              label: const Text('Upload')
             ),
           ],
         ),
