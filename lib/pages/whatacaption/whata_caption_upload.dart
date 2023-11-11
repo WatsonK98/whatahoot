ddcd91f16ab2a4dce895300cd96613dd3a6bd7b1
aswatson <aswatson95@gmail.com>
2023-11-11 11:13:13 -0700

    adding comments

--- lib/pages/whatacaption/whata_caption_upload.dart
+++ lib/pages/whatacaption/whata_caption_upload.dart
@@ -5,31 +5,39 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'whata_caption_caption.dart';
 import 'dart:io';
 
+///Whatacaption upload page starting point
 class WhataCaptionUploadPage extends StatefulWidget{
   const WhataCaptionUploadPage({super.key});
 
+  ///Initialize the page state
   @override
   State<WhataCaptionUploadPage> createState() => _WhataCaptionUploadPageState();
 }
 
+///Upload page state
 class _WhataCaptionUploadPageState extends State<WhataCaptionUploadPage>{
-  final TextEditingController _textEditingController = TextEditingController();
   final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
   final storageRef = FirebaseStorage.instance.ref();
   static File? _imageFile;
+  bool _imageUploaded = false;
 
+  ///sends the image file to cloud storage
   Future<void> _findImageFile() async {
     SharedPreferences prefs = await _prefs;
+
     String? serverId = prefs.getString('joinCode');
     String? playerId = prefs.getString('playerId');
+
     ImagePicker picker = ImagePicker();
     XFile? image = await picker.pickImage(source: ImageSource.gallery);
     if (image == null) {
       return;
     }
+
     setState(() {
       _imageFile = File(image.path);
     });
+
     final imageRef = storageRef.child("$serverId/$playerId");
     try {
       UploadTask uploadTask = imageRef.putFile(_imageFile!);
@@ -44,12 +52,7 @@ class _WhataCaptionUploadPageState extends State<WhataCaptionUploadPage>{
     }
   }
 
-  @override
-  void dispose() {
-    _textEditingController.dispose();
-    super.dispose();
-  }
-
+  ///Page widget
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -61,12 +64,13 @@ class _WhataCaptionUploadPageState extends State<WhataCaptionUploadPage>{
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            //Image file container
             const SizedBox(height: 10),
             Center(
               child: _imageFile != null
                       ? Image.memory(
                     _imageFile!.readAsBytesSync(),
-                    scale: .1,
+                    scale: .5,
                     fit: BoxFit.cover,
                   )
                   : Container(height: 300),
@@ -75,19 +79,24 @@ class _WhataCaptionUploadPageState extends State<WhataCaptionUploadPage>{
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
+                //upload button sends the image to the cloud
                 ElevatedButton.icon(
                     onPressed: () {
-                      _findImageFile().then((_) => null);
+                      _findImageFile().then((_) => _imageUploaded == true);
                     },
                     icon: const Icon(Icons.cloud_upload),
                     label: const Text('Upload')
                 ),
                 const SizedBox(width: 10),
                 ElevatedButton(
+                    //
                     onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(
-                              builder: (context) => const WhataCaptionCaptionPage()));
+                      //Only move on if an image was uploaded
+                      if (_imageUploaded) {
+                        Navigator.push(context,
+                            MaterialPageRoute(
+                                builder: (context) => const WhataCaptionCaptionPage()));
+                      }
                     },
                     child: const Text('Continue')
                 ),
